ca1e3e0522c002884f45d678a810be1df7e615d8
mistyshanshui <mistyshanshui@outlook.com>
2022-03-27 11:47:18 -0700

    3.6 error handling

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -28,36 +28,44 @@ let persons = [
     }
 ]
 
-app.get('/api/persons', (request, response)=>{
+app.get('/api/persons', (request, response) => {
     response.send(persons)
 })
 
-app.get('/info', (request, response)=>{
+app.get('/info', (request, response) => {
     response.send(`phone book has info for ${persons.length} people <br/> ${Date()}`)
 })
 
-app.get('/api/persons/:id', (request, response)=>{
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(p => p.id === id)
-    if(person){
+    if (person) {
         response.json(person)
     }
-    else{
+    else {
         response.status(404).end()
     }
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(p => p.id !== id)
     response.status(204).end()
 })
 
-app.post('/api/persons', (request, response)=>{
+app.post('/api/persons', (request, response) => {
     const person = request.body
-    person.id = Math.floor(Math.random() * 100000000)
-    persons = persons.concat(person)
-    response.send(person)
+    if (person.number == null || person.name == null) {
+        response.status(400).send(`{error : 'person must have name and number}`)
+    }
+    else if (persons.find(p => p.name === person.name)) {
+        response.status(400).send(`{error: 'name must be unique'}`)
+    }
+    else {
+        person.id = Math.floor(Math.random() * 100000000)
+        persons = persons.concat(person)
+        response.send(person)
+    }
 })
 
 const PORT = 3001
