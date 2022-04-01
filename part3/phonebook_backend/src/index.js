3fb0ab78be866ee4d6ff8cf3f88481328e27cc07
mistyshanshui <mistyshanshui@outlook.com>
2022-04-01 11:28:21 -0700

    3.15 - 3.18 add error handling

--- part3/phonebook_backend/src/index.js
+++ part3/phonebook_backend/src/index.js
@@ -28,18 +28,18 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(results=>{
+    Person.find({}).then(results => {
         response.send(`phone book has info for ${results.length} people <br/> ${Date()}`)
     })
 })
 
 app.get('/', (request, response) => {
-    Person.find({}).then(results=>{
+    Person.find({}).then(results => {
         response.send(`phone book has info for ${results.length} people <br/> ${Date()}`)
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             if (person) {
@@ -49,14 +49,16 @@ app.get('/api/persons/:id', (request, response) => {
                 response.status(404).end()
             }
         })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             console.log(result)
             response.status(204).end()
         })
+        .catch(error=>next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -69,14 +71,30 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const person = { ...request.body }
     Person.findByIdAndUpdate(person.id, person, { new: true })
         .then(updatedPerson => {
             console.log(updatedPerson)
             response.send(updatedPerson)
         })
+        .catch(error=>next(error))
 })
+
+const unknownEndpoint = (request, response) =>{
+    response.status(404).send({error : 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) =>{
+    console.error(error.message)
+    if(error.name == "CastError"){
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
