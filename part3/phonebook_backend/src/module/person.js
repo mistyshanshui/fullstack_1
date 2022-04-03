ccc071f869f7bc243245608a9f39044a69dc1873
mistyshanshui <mistyshanshui@outlook.com>
2022-04-03 11:29:27 -0700

    3.20 regexp for phone number

--- part3/phonebook_backend/src/module/person.js
+++ part3/phonebook_backend/src/module/person.js
@@ -18,7 +18,12 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required : true
+        minLength: 8,
+        validate: {
+            validator: v => /\d{2,3}-\d{7}/.test(v),
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: true
     }
 })
 
