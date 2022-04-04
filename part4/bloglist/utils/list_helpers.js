905021821e156937e8990f04c272a8d660df85dc
mistyshanshui <mistyshanshui@outlook.com>
2022-04-04 14:01:45 -0700

    4.4 test total likes

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -3,6 +3,21 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {
-    dummy
+
+const countLikes = (blogs) => {
+    if (blogs.length == 0) {
+        return 0
+    }
+    else if (blogs.length == 1) {
+        return blogs[0].likes
+    }
+    else {
+        let count = 0
+        blogs.forEach(element => {
+            count += element.likes
+        });
+        return count
+    }
 }
+module.exports = { dummy, countLikes }
+
