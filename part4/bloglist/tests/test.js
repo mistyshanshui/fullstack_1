e1026eb7527a0feb853bcb496fc7420709400c4a
mistyshanshui <mistyshanshui@outlook.com>
2022-04-04 13:44:27 -0700

    4.3 add unit test

---
+++ part4/bloglist/tests/test.js
@@ -0,0 +1,7 @@
+const listHelpers = require('../utils/list_helpers')
+
+test('dummy returns one', () => {
+    const blogs = []
+    const result = listHelpers.dummy(blogs)
+    expect(result).toBe(1)
+})
